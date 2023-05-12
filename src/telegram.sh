3bf0e9d7743fbdd01a2cb5f27e6a5fa4a6476377
Mamie Malwyn <mamiemal@pr-n5.kzn.21-school.ru>
2023-05-12 18:19:28 +0300

    notify

--- src/telegram.sh
+++ src/telegram.sh
@@ -1,2 +1,10 @@
-fdsfsscdasd
-sdasdzdadsadsds
+#!/bin/bash
+
+TELEGRAM_BOT_TOKEN="5967020997:AAE_mz0KP5PDw_4dCfx0MkfXA8vEziIUjzs"
+TELEGRAM_USER_ID="1440030788"
+
+URL="https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendMessage"
+TEXT="Deploy status: $1%0A%0AProject:+$CI_PROJECT_NAME%0AStatus:+$CI_JOB_STATUS%0AURL:+$CI_PROJECT_URL/pipelines/$CI_PIPELINE_ID/%0ABranch:+$CI_COMMIT_REF_SLUG"
+
+
+curl -s -d "chat_id=$TELEGRAM_USER_ID&disable_web_page_preview=1&text=$TEXT" $URL > /dev/null
