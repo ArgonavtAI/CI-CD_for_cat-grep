d6a7762db2b79d427d042c4614780bb8df965870
Mamie Malwyn <mamiemal@pr-n5.kzn.21-school.ru>
2023-05-12 17:31:08 +0300

    deploy_3

--- src/copy.sh
+++ src/copy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-scp /home/gitlab-runner/builds/-LJkxDsk/0/students/DO6_CICD.ID_356283/mamiemal_student.21_school.ru/DO6_CICD-2/src/cat/s21_cat mami@10.10.0.2:/usr/local/bin/
-
+s
+cp /home/gitlab-runner/builds/-LJkxDsk/0/students/DO6_CICD.ID_356283/mamiemal_student.21_school.ru/DO6_CICD-2/src/cat/s21_cat mami@10.10.0.2:/usr/local/bin/
 scp /home/gitlab-runner/builds/-LJkxDsk/0/students/DO6_CICD.ID_356283/mamiemal_student.21_school.ru/DO6_CICD-2/src/grep/s21_grep mami@10.10.0.2:/usr/local/bin/ 
 
 ssh mami@10.10.0.2 ls /usr/local/bin
